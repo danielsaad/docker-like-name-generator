acd47b010ab233675224dc2ba03a3a0af19c3def
Daniel Saad Nogueira Nunes <daniel.saad.nunes@gmail.com>
2018-03-12 22:29:11 -0300

    adding turing award winners

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 function getRandomName(retry, noSpace) {
-    if (noSpace === void 0) { noSpace = false; }
+    if (noSpace === void 0) { noSpace = true; }
     var left = ["admiring", "adoring", "agitated", "amazing", "angry", "awesome", "backstabbing", "berserk", "big", "boring", "clever", "cocky", "compassionate", "condescending", "cranky", "desperate", "determined", "distracted", "dreamy", "drunk", "ecstatic", "elated", "elegant", "evil", "fervent", "focused", "furious", "gigantic", "gloomy", "goofy", "grave", "happy", "high", "hopeful", "hungry", "insane", "jolly", "jovial", "kickass", "lonely", "loving", "mad", "modest", "naughty", "nauseous", "nostalgic", "pedantic", "pensive", "prickly", "reverent", "romantic", "sad", "serene", "sharp", "sick", "silly", "sleepy", "small", "stoic", "stupefied", "suspicious", "tender", "thirsty", "tiny", "trusting"];
     // Turing award prizes until 2017
     var turing_award = ["adleman","allen","bachman", "backus","berners-lee", "blum", "brooks", 
